997dad196d0616acb902e6d5af2b107a69320e3b
Helder Carvalho <32930044+HelderMCarvalho@users.noreply.github.com>
2018-10-22 23:26:30 +0100

    ADD Comentários explicativos (MODIFY app.js, scripts.js) | FINISH

--- NodejsChatServer/app.js
+++ NodejsChatServer/app.js
@@ -12,118 +12,114 @@ var buffer = require('buffer');
 aplicacao.use(express.static(__dirname + '/public'));
 //FIM ROTAS
 
-var utilizadoresLigados = [];
-var nomeUtilizadoresLigados = [];
+var utilizadoresLigados = []; //Guarda os sockets ligados
+var nomeUtilizadoresLigados = []; //Guarda apenas os nomes de utilizadores ligados
 
-socket.on('connection', function (socket2) {
-    var utilizador = { nome: 'GUEST', password: '' };
-    socket2.utilizador = utilizador;
-    utilizadoresLigados.push(socket2);
-    nomeUtilizadoresLigados.push(utilizador.nome);
-    socket.emit('atualizarUtilizadoresLigados', nomeUtilizadoresLigados);
+socket.on('connection', function (socket2) { //Esta função é executada quando algum utilizador se conecta ao servidor
+    var utilizador = { nome: 'GUEST', password: '' }; //Cria o objeto local "utilizador" (como acabou de se conectar é um "GUEST")
+    socket2.utilizador = utilizador; //Guarda no socket a informação do utilizador
+    utilizadoresLigados.push(socket2); //Insere o socket do utilizador que se ligou nos "utilizadores ligados"
+    nomeUtilizadoresLigados.push(utilizador.nome); //Insere apenas o nome do utilizador que se ligou nos "nome utilizadores liagdos"
+    socket.emit('atualizarUtilizadoresLigados', nomeUtilizadoresLigados); //Envia a lista dos utilizadores ligados
     console.log('Utilizador Ligado! TOTAL: ' + utilizadoresLigados.length);
-    socket2.on('disconnect', function () {
-        utilizadoresLigados.splice(utilizadoresLigados.indexOf(socket2), 1);
-        nomeUtilizadoresLigados.splice(nomeUtilizadoresLigados.indexOf(utilizador.nome), 1);
-        socket.emit('atualizarUtilizadoresLigados', nomeUtilizadoresLigados);
+    socket2.on('disconnect', function () { //Esta função é executada quando algum utilizador se desconecta do servidor
+        utilizadoresLigados.splice(utilizadoresLigados.indexOf(socket2), 1); //Retira o socket do utilizador que se desligou dos "utilizadores ligados"
+        nomeUtilizadoresLigados.splice(nomeUtilizadoresLigados.indexOf(utilizador.nome), 1); //Retira apenas o nome do utilizador que se desligou dos "nome utilizadores liagdos"
+        socket.emit('atualizarUtilizadoresLigados', nomeUtilizadoresLigados); //Envia a lista dos utilizadores ligados
         console.log('Utilizador desligado! TOTAL: ' + utilizadoresLigados.length);
-    }).on('envioMensagemServidor', function (mensagem) {
-        utilizadoresLigados.forEach(function (uti) {
-            if (uti === socket2) {
+    }).on('envioMensagemServidor', function (mensagem) { //Envia a mensagem de um utilizador para todos os outros menos para o que a enviou ("mensagem" é a mensagem a ser enviada)
+        utilizadoresLigados.forEach(function (uti) { //Percorre os utilizadores ligados ("uti" será cada elemento dos "utilizadores ligados")
+            if (uti === socket2) { //Verifica se o utilizador atualmente percorrido é igual o que enviou a mensagem
+                return; //Caso seja, este "return" fará com que seja passado à frente sem executar mais código
+            }
+            uti.emit('envioMensagemCliente', { utilizador: utilizador.nome, msg: mensagem }); //Envia a mensagem para o utilizador "uti"
+        });
+    }).on('envioMensagemPrivadaServidor', function (dados) { //Envia a mensagem de um utilizador apenas para 1 destinatário ("dados" é um objeto composto pelo nome de utilizador de destino e pela mensagem)
+        utilizadoresLigados.forEach(function (uti) { //Percorre os utilizadores ligados ("uti" será cada elemento dos "utilizadores ligados")
+            if (uti.utilizador.nome == dados.destino) { //Verifica se o utilizador atualmente percorrido é igual ou utilizador para o qual a mensagem é destinada
+                uti.emit('envioMensagemPrivadaCliente', { utilizador: utilizador.nome, msg: dados.msg }); //Envia a mensagem para o utilizador "uti" (neste caso é o utilizador destino)
                 return;
             }
-            uti.emit('envioMensagemCliente', { utilizador: utilizador.nome, msg: mensagem });
         });
-    }).on('envioMensagemPrivadaServidor', function (dados) {
-        utilizadoresLigados.forEach(function (uti) {
-            if (uti.utilizador.nome == dados.destino) {
-                uti.emit('envioMensagemPrivadaCliente', { utilizador: utilizador.nome, msg: dados.msg });
-                return;
-            }
-        });
-    }).on('registarUtilizador', function (uti) {
-        //FUNÇÃO REGISTAR UTILIZADORES
-        var existe = 0; //0 - Não existe | 1 - Existe
+    }).on('registarUtilizador', function (uti) { //Regista utilizador caso esse nome de utilizador esteja disponível ("uti" é o objeto recebido do formulário de registo que contem o nome de utilizador e password)
+        var existe = 0; //0 - Não existe; 1 - Existe
         //LER UTILIZADORES DO FICHEIRO E VERIFICAR SE JÁ EXISTE ALGUM COM O MESMO NOME
-        var rl = readline.createInterface({
-            input: fs.createReadStream('utilizadores.txt'),
+        var rl = readline.createInterface({ //Cria uma "Interface" do módulo "ReadLine"
+            input: fs.createReadStream('utilizadores.txt'), //Ficheiro que vai ser lido
             crlfDelay: Infinity
-        }).on('line', function (line) {
-            if (uti.nome == line) {
-                existe = 1;
+        }).on('line', function (line) { //Percorre o ficheiro linha a linha
+            if (uti.nome == line) { //Verifica se o nome de utilizador recebido é igual à linha que está neste momento a ser percorrida
+                existe = 1; //Caso seja, "existe" passa a ter o valor de 1 (o que quer dizer que já existe um utilizador registado com esse nome)
                 rl.close();
             }
-        }).on('close', function () {
-            if (existe == 0) {
-                utilizador = uti;
+        }).on('close', function () { //Quando o ficheiro acabar de ser lido
+            if (existe == 0) { //Verifica "existe" (se for "=0" quer dizer que não existe nenhum utilizador registado com esse nome e que pode ser criado | se for "=1" quer dizer que não pode ser criado)
                 var buff = new Buffer(uti.password);
-                var buffBase = buff.toString('base64');
-                fs.appendFile('utilizadores.txt', uti.nome + '\r\n' + buffBase + '\r\n\r\n', (err) => {
+                var buffBase = buff.toString('base64'); //A password é codificada
+                fs.appendFile('utilizadores.txt', uti.nome + '\r\n' + buffBase + '\r\n\r\n', (err) => { //As informações do novo utilizador são escritas o ficheiro
                     if (err) {
                         throw err;
                     }
                     console.log('Utilizador criado!');
-                    socket2.emit('redirect', '/'); //Registou com sucesso
+                    socket2.emit('confirmacaoRegistarUtilizador', 1); //Envia a confirmação positiva de registo
                 });
             }
             else {
-                socket2.emit('confirmacaoRegistarUtilizador', 0); //Não registou
+                socket2.emit('confirmacaoRegistarUtilizador', 0); //Envia a confirmação negativa de registo
             }
         });
         //FIM LER UTILIZADORES DO FICHEIRO E VERIFICAR SE JÁ EXISTE ALGUM COM O MESMO NOME
-    }).on('loginUtilizador', function (uti) {
-        var existe = 0, passcorreta = 0; //0 - Não existe | 1 - Existe
-        //LER UTILIZADORES DO FICHEIRO E VERIFICAR SE JÁ EXISTE ALGUM REGISTADO COM ESSE NOME
-        var readStream = fs.createReadStream('utilizadores.txt');
-        var rl = readline.createInterface({
-            input: readStream,
+    }).on('loginUtilizador', function (uti) { //Faz login do utilizador caso exista e tenha introduzido a password correta ("uti" é o objeto recebido do formulário de login que contem o nome de utilizador e password)
+        var existe = 0, passcorreta = 0; //0 - Não existe; 1 - Existe | 0 - Não introduziu a password correta;  1 - Introduziu a password correta
+        //LER UTILIZADORES DO FICHEIRO E VERIFICAR SE EXISTE ALGUM REGISTADO COM ESSE NOME E COM ESSA PASSWORD
+        var rl = readline.createInterface({ //Cria uma "Interface" do módulo "ReadLine"
+            input: fs.createReadStream('utilizadores.txt'), //Ficheiro que vai ser lido
             crlfDelay: Infinity
-        }).on('line', function (line) {
-            if (existe == 0) {
-                if (uti.nome == line) {
-                    existe = 1;
+        }).on('line', function (line) { //Percorre o ficheiro linha a linha
+            if (existe == 0) { //Verifica "existe" (se for "=0" quer dizer que ainda não encontrou nenhum utilizador com esse nome e vai continuar a procurar até ao fim do ficheiro)
+                if (uti.nome == line) { //Verifica se o nome de utilizador recebido é igual à linha que está neste momento a ser percorrida
+                    existe = 1; //Caso seja, "existe" passa a ter o valor de 1 (o que quer dizer que não serão verificados mais nomes e que pode ser verificada a password)
                 }
-            } else if (existe == 1) {
+            } else if (existe == 1) { //Verifica "existe" (se for "=1" quer dizer que existe 1 utilizador registado com esse nome e que pode ser verificada a password)
                 var buff = new Buffer(line, 'base64');
-                var password = buff.toString('ascii');
-                if (uti.password == password) {
-                    passcorreta = 1;
+                var password = buff.toString('ascii'); //A password é descodificada
+                if (uti.password == password) { //Verifica se a password recebida é igual à password descodificada
+                    passcorreta = 1; //Caso seja, "passcorreta" passa a ter o valor de 1 (o que quer dizer que o utilizador existe e introduziu a password correta)
                     rl.close();
                 } else {
                     rl.close();
                 }
             }
-        }).on('close', function () {
-            if (existe == 0 || passcorreta == 0) {
-                socket2.emit('confirmacaoLoginUtilizador', 0); //Não fez login
-            }
-            else if (existe == 1 && passcorreta == 1) {
-                nomeUtilizadoresLigados.splice(nomeUtilizadoresLigados.indexOf('GUEST'), 1);
-                utilizador = uti;
-                socket2.utilizador = utilizador;
-                socket2.emit('confirmacaoLoginUtilizador', 1); //Fez login
-                nomeUtilizadoresLigados.push(utilizador.nome);
-                socket.emit('atualizarUtilizadoresLigados', nomeUtilizadoresLigados);
+        }).on('close', function () { //Quando o ficheiro acabar de ser lido
+            if (existe == 0 || passcorreta == 0) { //Verifica "existe" e "passcorreta" (se 1 dos 2 for "=0" quer dizer que não introduziu o nome de utilizador ou a password corretos)
+                socket2.emit('confirmacaoLoginUtilizador', 0); //Envia a confirmação negativa de login
+            } else if (existe == 1 && passcorreta == 1) { //Verifica "existe" e "passcorreta" (se os 2 forem "=1" quer dizer que introduziu o nome de utilizador e a password corretos)
+                nomeUtilizadoresLigados.splice(nomeUtilizadoresLigados.indexOf('GUEST'), 1); //Retira um "GUEST" da lista de "nome utilizadores ligados"
+                utilizador = uti; //Guarda a informação do utilizador que fez login no objeto local "utilizador"
+                socket2.utilizador = utilizador; //Guarda no socket a informação do utilizador que fez login
+                socket2.emit('confirmacaoLoginUtilizador', 1); //Envia a confirmação positiva de login
+                nomeUtilizadoresLigados.push(utilizador.nome); //Insere apenas o nome do novo utilizador que fez login nos "nome utilizadores liagdos"
+                socket.emit('atualizarUtilizadoresLigados', nomeUtilizadoresLigados); //Envia a lista dos utilizadores ligados
             }
         });
-        //FIM LER UTILIZADORES DO FICHEIRO E VERIFICAR SE JÁ EXISTE ALGUM REGISTADO COM ESSE NOME
-    }).on('comecaEscrever', function () {
-        utilizadoresLigados.forEach(function (uti) {
-            if (uti === socket2) {
-                return;
+        //FIM LER UTILIZADORES DO FICHEIRO E VERIFICAR SE EXISTE ALGUM REGISTADO COM ESSE NOME E COM ESSA PASSWORD
+    }).on('comecaEscrever', function () { //Envia o aviso de que 1 utilizador começou a escrever para todos os outros menos para o que começou a escrever
+        utilizadoresLigados.forEach(function (uti) { //Percorre os utilizadores ligados ("uti" será cada elemento dos "utilizadores ligados")
+            if (uti === socket2) { //Verifica se o utilizador atualmente percorrido é igual o que começou a escrever
+                return; //Caso seja, este "return" fará com que seja passado à frente sem executar mais código
             }
-            uti.emit('mostrarEscrever', utilizador.nome);
+            uti.emit('mostrarEscrever', utilizador.nome); //Envia o nome do utilizador que começou a escrever
         });
-    }).on('parouEscrever', function () {
-        utilizadoresLigados.forEach(function (uti) {
-            if (uti === socket2) {
-                return;
+    }).on('parouEscrever', function () { //Envia o aviso de que 1 utilizador parou de escrever para todos os outros menos para o que parou de escrever
+        utilizadoresLigados.forEach(function (uti) { //Percorre os utilizadores ligados ("uti" será cada elemento dos "utilizadores ligados")
+            if (uti === socket2) { //Verifica se o utilizador atualmente percorrido é igual o que parou de escrever
+                return; //Caso seja, este "return" fará com que seja passado à frente sem executar mais código
             }
-            uti.emit('retirarEscrever', utilizador.nome);
+            uti.emit('retirarEscrever', utilizador.nome); //Envia o nome do utilizador que parou de escrever
         });
     });
 });
 
-http.listen(5000, function () {
+http.listen(5000, function () { //Porta em que o servidor está a ser executado
     console.log('A espera de ligacoes!');
 });
--- NodejsChatServer/public/scripts.js
+++ NodejsChatServer/public/scripts.js
@@ -1,102 +1,97 @@
 ﻿$(document).ready(function () {
     var socket = io();
-    var utilizador = 'GUEST';
+    var utilizador = 'GUEST'; //Nome de utilizador padrão
     //ENVIAR MENSAGEM
-    $('form#mensagem').submit(function () {
-        if ($('#destino').val() == 'Todos') {
-            socket.emit('envioMensagemServidor', $('#inputMensagem').val());
-            $('#listaMensagens').append($('<li>').text(utilizador + ': ' + $('#inputMensagem').val()));
-        } else {
-            socket.emit('envioMensagemPrivadaServidor', { destino: $('#destino').val(), msg: $('#inputMensagem').val() });
-            $('#listaMensagens').append($('<li>').text('Privada para ' + $('#destino').val() + ': ' + $('#inputMensagem').val()));
+    $('form#mensagem').submit(function () { //Esta função é executada quando o utilizador enviar a mensagem
+        if ($('#destino').val() == 'Todos') { //Verifica se o valor do campo "destino" (para quem a mensagem é destinada) é "==Todos", caso seja quer dizer que esta mensagem é para ser enviada para todos os utilizadores
+            socket.emit('envioMensagemServidor', $('#inputMensagem').val()); //Envia a mensagem para o servidor
+            $('#listaMensagens').append($('<li>').text(utilizador + ': ' + $('#inputMensagem').val())); //Adiciona a mensagem que o utilizador acabou de enviar na "lista de mensagens"
+        } else { //Este "else" será executado caso o campo "destino" tenha um valor "!=Todos" (o que quer dizer que a mensagem é para ser enviada apenas para o utilizador que estiver selecionado no campo "destino")
+            socket.emit('envioMensagemPrivadaServidor', { destino: $('#destino').val(), msg: $('#inputMensagem').val() }); //Envia um objeto, composto pelo utilizador de destino e pela mensagem, para o servidor
+            $('#listaMensagens').append($('<li>').text('Privada para ' + $('#destino').val() + ': ' + $('#inputMensagem').val())); //Adiciona a mensagem que o utilizador acabou de enviar na "lista de mensagens"
         }
-        $('#inputMensagem').val('');
-        $("#mensagens").scrollTop($("#mensagens")[0].scrollHeight);
+        $('#inputMensagem').val(''); //Limpa o campo de mensagens
+        $("#mensagens").scrollTop($("#mensagens")[0].scrollHeight); //Faz scoll para cima da lista de mensagens (para vermos a mensagem acabada de enviar)
         return false;
     });
     //FIM ENVIAR MENSAGEM
 
     //ENVIAR ESTADO DE COMEÇOU/PAROU DE ESCREVER
-    var tem = false;
-    var vazio = true;
-    $("#inputMensagem").keyup(function () {
-        if (($("#inputMensagem").val().length >= 1) && (tem == false)) {
-            tem = true;
-            vazio = false;
-            console.log('escreve');
-            socket.emit('comecaEscrever');
-        } else if (($("#inputMensagem").val() == '') && (vazio == false)) {
-            tem = false;
-            vazio = true;
-            console.log('não escreve');
-            socket.emit('parouEscrever');
+    var tem = false; //false - O campo de mensagem não tem conteúdo; true - O campo de mensagens tem conteúdo
+    //(Esta última variável não é totalmente necessária para a funcionalidade de "ENVIAR ESTADO DE COMEÇOU/PAROU DE ESCREVER" mas ajuda porque fará com que o servidor não receba um evento cada vez que o utilizador pressione uma tecla)
+    $("#inputMensagem").keyup(function () { //Esta função é executada cada vez que o utilizador pressionar uma tecla tendo o campo de "mensagem" selecionado
+        if (($("#inputMensagem").val().length >= 1) && (tem == false)) { //Verifica se o campo de "mensagem" tem 1 ou + de 1 caráter e se anteriormente estava vazio (no fundo, este "if" verifica quando o utilizador começa a escrever no campo vazio)
+            tem = true; //Caso o que está escrito acima seja verdade, quer dizer que o campo já tem algo escrito logo "tem" passa a ser "true"
+            socket.emit('comecaEscrever'); //Envia o aviso de que o utilizador começou a escrever
+        } else if (($("#inputMensagem").val() == '') && (tem == true)) { //Verifica se o campo de "mensagem" está vazio e se anteriormente não estava vazio (no fundo, este "if" verifica quando o utilizador tem o campo vazio sendo que anteriormente estava cheio)
+            tem = false; //Caso o que está escrito acima seja verdade, quer dizer que o campo passa a não ter nada escrito logo "tem" passa a ser "false"
+            socket.emit('parouEscrever'); //Envia o aviso de que o utilizador parou de escrever
         }
     });
     //FIM DE ENVIAR ESTADO DE COMEÇOU/PAROU DE ESCREVER
 
     //REGISTAR
-    $('form#registarUtilizador').submit(function () {
-        $('h3.erroLoginRegisto').css('display', 'none');
-        socket.emit('registarUtilizador', { nome: $('#inputUtilizador').val(), password: $('#inputPassword').val() });
-        return false;
+    $('form#registarUtilizador').submit(function () { //Esta função é executada quando o utilizador envia o formulário de registo
+        $('h3.erroLoginRegisto').css('display', 'none'); //Esconde a mensagem de erro de registo (caso ela já esteja à vista)
+        socket.emit('registarUtilizador', { nome: $('#inputUtilizador').val(), password: $('#inputPassword').val() }); //Envia um objeto, composto pelo nome de utilizador e pela password, para o servidor
     });
-    socket.on('confirmacaoRegistarUtilizador', function (flag) {
-        if (flag == 0) {
-            $('h3.erroLoginRegisto').css('display', 'block');
+    socket.on('confirmacaoRegistarUtilizador', function (flag) { //Recebe a confirmação de que o utilizador foi ou não registado ("flag" pode tomar o valor de 0 - Não foi registado; 1 - Foi registado)
+        if (flag == 0) { //Verifica "flag" (se for "=0" quer dizer que não foi registado)
+            $('h3.erroLoginRegisto').css('display', 'block'); //Mostra a mensagem de erro
+        } else if (flag == 1) { //Verifica "flag" (se for "=1" quer dizer que foi registado)
+            window.location.href = '/'; //Redireciona o utilizador para a página de chat onde poderá vazer o login
         }
-    });
-    socket.on('redirect', function (destino) {
-        window.location.href = destino;
     });
     //FIM REGISTAR
 
     //LOGIN
-    $('form#loginUtilizador').submit(function () {
-        $('h3.erroLoginRegisto').css('display', 'none');
-        socket.emit('loginUtilizador', { nome: $('#inputUtilizador').val(), password: $('#inputPassword').val() });
+    $('form#loginUtilizador').submit(function () { //Esta função é executada quando o utilizador envia o formulário de login
+        $('h3.erroLoginRegisto').css('display', 'none'); //Esconde a mensagem de erro do login (caso ela já esteja à vista)
+        socket.emit('loginUtilizador', { nome: $('#inputUtilizador').val(), password: $('#inputPassword').val() }); //Envia um objeto, composto pelo nome de utilizador e pela password, para o servidor
         return false;
     });
-    socket.on('confirmacaoLoginUtilizador', function (flag) {
-        if (flag == 0) {
-            $('h3.erroLoginRegisto').css('display', 'block'); //Não fez login
-        } else if (flag == 1) {
-            utilizador = $('#inputUtilizador').val();
-            $('div#loginHeader').css('display', 'none'); //Fez login
-            $('#utilizadorLigado').text('Bem vindo ' + utilizador + '!');
-            $('div#logoutHeader').css('display', 'block');
+    socket.on('confirmacaoLoginUtilizador', function (flag) { //Recebe a confirmação de que o utilizador fez ou não o login com sucesso ("flag" pode tomar o valor de 0 - Não fez login com sucesso; 1 - Fez login com sucesso)
+        if (flag == 0) { //Verifica "flag" (se for "=0" quer dizer que não fex login)
+            $('h3.erroLoginRegisto').css('display', 'block'); //Mostra a mensagem de erro
+        } else if (flag == 1) { //Verifica "flag" (se for "=1" quer dizer que fez login)
+            utilizador = $('#inputUtilizador').val(); //Muda o nome de utilizador para o valor do campo "utilizador"
+            $('div#loginHeader').css('display', 'none'); //Esconde o formulário de login e o botão de registo
+            $('#utilizadorLigado').text('Bem vindo ' + utilizador + '!'); //Cria uma mensagem de boas-vindas
+            $('div#logoutHeader').css('display', 'block'); //Mostra o botão de logout
         }
     });
     //FIM LOGIN
 
     //RECEBER MENSAGEM DO SERVIDOR
-    socket.on('envioMensagemCliente', function (mensagem) {
-        $('#listaMensagens').append($('<li>').text(mensagem.utilizador + ': ' + mensagem.msg));
+    socket.on('envioMensagemCliente', function (mensagem) { //Recebe uma mensagem e coloca-a na lista de mensagens ("mensagem" é um objeto composto pelo autor da mensagem e pela mensagem)
+        $('#listaMensagens').append($('<li>').text(mensagem.utilizador + ': ' + mensagem.msg)); //Adiciona a mensagem recebida na "lista de mensagens"
     });
-    socket.on('envioMensagemPrivadaCliente', function (mensagem) {
-        $('#listaMensagens').append($('<li>').text('Privada de ' + mensagem.utilizador + ': ' + mensagem.msg));
+    socket.on('envioMensagemPrivadaCliente', function (mensagem) { //Recebe uma mensagem e coloca-a na lista de mensagens ("mensagem" é um objeto composto pelo autor da mensagem e pela mensagem)
+        $('#listaMensagens').append($('<li>').text('Privada de ' + mensagem.utilizador + ': ' + mensagem.msg)); //Adiciona a mensagem recebida na "lista de mensagens"
     });
+    //FIM RECEBER MENSAGEM DO SERVIDOR
 
-    //RECEBER QUEM ESTÁ A ESCREVER
-    socket.on('mostrarEscrever', function (utilizadorEscreve) {
-        $('#listaEscrever').append($('<li id="escrever' + utilizadorEscreve + '">').text(utilizadorEscreve + ' está a escrever!'));
+    //RECEBER QUEM COMEÇOU/PAROU DE ESCREVER
+    socket.on('mostrarEscrever', function (utilizadorEscreve) { //Recebe e mostra o utilizador que começou a escrever ("utilizadorEscreve" é o nome do utilizador que começou a escrever)
+        $('#listaEscrever').append($('<li id="escrever' + utilizadorEscreve + '">').text(utilizadorEscreve + ' está a escrever!'));  //Adiciona o aviso de que algem começou a escrever na "lista de escrever"
     });
-
-    //RECEBER QUEM PAROU DE ESCREVER
-    socket.on('retirarEscrever', function (utilizadorParouEscrever) {
-        var id = '#escrever' + utilizadorParouEscrever;
-        $(id).remove();
+    socket.on('retirarEscrever', function (utilizadorParouEscrever) { //Recebe e retira o utilizador que parou de escrever ("utilizadorParouEscrever" é o nome do utilizador que parou de escrever)
+        var id = '#escrever' + utilizadorParouEscrever; //Constroi o "id" do elemento da lista que vai ser removido
+        $(id).remove(); //Remove esse elemento
     });
+    //FIM RECEBER QUEM COMEÇOU/PAROU DE ESCREVER
 
     //ATUALIZAR LISTA DE UTILIZADORES LIGADOS
-    socket.on('atualizarUtilizadoresLigados', function (nomesUtilizadoresLigados) {
-        $('#listautilizadoresLigados').empty();
-        $('#destino').empty();
-        $('#destino').append($('<option value="Todos">').text('TODOS'));
-        $.each(nomesUtilizadoresLigados, function (key, value) {
-            $('#listautilizadoresLigados').append($('<li>').text(value));
-            if (value != 'GUEST' && value != utilizador) {
-                $('#destino').append($('<option value="' + value + '">').text(value));
-            }
+    socket.on('atualizarUtilizadoresLigados', function (nomesUtilizadoresLigados) { //Recebe e mostra os utilizadores ligados ("nomesUtilizadoresLigados" é um array com todos os nomes de utilizadores que estão neste momento ligados)
+        $('#listautilizadoresLigados').empty(); //Limpa a atual lista de "utilizadores ligados"
+        $('#destino').empty(); //Limpa a caixa de seleção de utilizador para enviar mensagem privada
+        $('#destino').append($('<option value="Todos">').text('TODOS')); //Adiciona o destino padrão (disponibiliza o envio de mensagens para todos os utilizadores que tem de estar sempre visível)
+        $.each(nomesUtilizadoresLigados, function (key, value) { //Percorre os nomes utilizadores ligados ("value" será o nome de cada elemento dos "nomes utilizadores ligados")
+            $('#listautilizadoresLigados').append($('<li>').text(value)); //Adiciona cada utilizador à lista de "utilizadores ligados"
+            if (value != 'GUEST' && value != utilizador) { //Verifica se nome de utilizador que está a ser percorrido neste momento é ("!=GUEST" e "!=" dele próprio)
+                $('#destino').append($('<option value="' + value + '">').text(value)); //Caso seja, adiciona à lista de "destino" esse nome de utilizador
+            } //(No fundo, este "if" fará com que sejam adicionados à lista de destino todos os nomes dos utilizadores ligados exceto os "GUEST" e o próprio utilizador)
         });
     });
+    //FIM ATUALIZAR LISTA DE UTILIZADORES LIGADOS
 });
